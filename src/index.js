450d2bf46dea8c39053bb41bf5fe071dcb1dc3eb
robore9197 <michelrobore1997@gmail.com>
2023-03-20 19:25:39 +0700

    3 files modified, 5 added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
